aa07831c92e1478e41ebcc113406b96950efc4da
Lucien Boillod <lucienboillod@gmail.com>
2018-04-12 13:17:49 +0100

    Update build.py (add settings, options ...)

--- build.py
+++ build.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bincrafters import build_template_default
+from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = ConanMultiPackager(username="impressivedev", channel="testing")
+    builder.add_common_builds()
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["compiler"] == "Visual Studio":
+            if settings["compiler.runtime"] == "MT" or settings["compiler.runtime"] == "MTd":
+                # Ignore MT runtime
+                continue
+        if settings["arch"] != "x86_64":
+            continue
 
+        filtered_builds.append([settings, options, env_vars, build_requires])
+
+    builder.builds = filtered_builds
     builder.run()
