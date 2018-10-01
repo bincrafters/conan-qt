199e38ca4cbd7976838998aa67c6f12ebd23709f
René Meusel <rene.meusel@nexenio.com>
2018-10-01 18:10:39 +0200

    FIX: use tools.get() on non-windows as well

--- conanfile.py
+++ conanfile.py
@@ -141,7 +141,7 @@ class QtConan(ConanFile):
         if tools.os_info.is_windows:
             tools.get("%s.zip" % url)
         else:
-            self.run("wget -qO- %s.tar.xz | tar -xJ " % url)
+            tools.get("%s.tar.xz" % url)
         shutil.move("qt-everywhere-src-%s" % self.version, "qt5")
         
         for patch in ["cc04651dea4c4678c626cb31b3ec8394426e2b25.diff", "ba22a6731377c8604d13e3855204c03652c0a2e3.diff"]:
