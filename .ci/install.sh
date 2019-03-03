88f33556ba1251ac36ad1e88d4bb9bbc4dd3457c
ericLemanissier <eric.lemanissier@gmail.com>
2019-03-03 09:10:02 +0100

    test recipe with meson too
    fix cmake subfolder
    fix appveyor path

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
-set -x
+set -ex
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
@@ -13,14 +12,13 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
     pyenv rehash
     pyenv activate conan
 fi
 
-pip install  --upgrade pip
-pip install  --upgrade conan
-pip install  conan_package_tools bincrafters_package_tools
+pip install conan --upgrade
+pip install conan_package_tools bincrafters_package_tools
 
 conan user
