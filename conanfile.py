0f96c50f2883f425879921359a7f3087e36fa9b1
ericLemanissier <eric.lemanissier@gmail.com>
2019-03-12 21:11:33 +0100

    qt 5.12.2

--- conanfile.py
+++ conanfile.py
@@ -44,7 +44,7 @@ class QtConan(ConanFile):
     _submodules = _getsubmodules()
 
     name = "qt"
-    version = "5.12.1"
+    version = "5.12.2"
     description = "Qt is a cross-platform framework for graphical user interfaces."
     topics = ("conan", "qt", "ui")
     url = "https://github.com/bincrafters/conan-qt"
@@ -280,14 +280,14 @@ class QtConan(ConanFile):
         url = "http://download.qt.io/official_releases/qt/{0}/{1}/single/qt-everywhere-src-{1}" \
             .format(self.version[:self.version.rfind('.')], self.version)
         if tools.os_info.is_windows:
-            tools.get("%s.zip" % url, sha256='18ebf2ea0f15a255512fe425abc00e6be0fc6960fd4eaa1189e85f81df650ce9')
+            tools.get("%s.zip" % url, sha256='1036bd65d067f70ba1384458d7a2daf574ceed36fa9321dcb3871d9810ef89e1')
         elif sys.version_info.major >= 3:
-            tools.get("%s.tar.xz" % url, sha256='caffbd625c7bc10ff8c5c7a27dbc7d84fa4de146975c0e1ffe904b514ccd6da4')
+            tools.get("%s.tar.xz" % url, sha256='59b8cb4e728450b21224dcaaa40eb25bafc5196b6988f2225c394c6b7f881ff5')
         else:  # python 2 cannot deal with .xz archives
             self.run("wget -qO- %s.tar.xz | tar -xJ " % url)
         shutil.move("qt-everywhere-src-%s" % self.version, "qt5")
 
-        for patch in ["cc04651dea4c4678c626cb31b3ec8394426e2b25.diff", "fffe5d622549f85968ea0be9717b90cbc020be71.diff", "f917000890e6360c92328ac6e3f052294e3a2959.diff"]:
+        for patch in ["cc04651dea4c4678c626cb31b3ec8394426e2b25.diff"]:
             tools.patch("qt5/qtbase", patch)
 
     def _xplatform(self):
