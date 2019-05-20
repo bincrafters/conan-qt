c4194281accc6f4e33c63627bd6bee313ce0ed18
ericLemanissier <eric.lemanissier@gmail.com>
2019-05-20 12:13:57 +0200

    fix libpq dep

--- build.py
+++ build.py
@@ -5,5 +5,5 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(build_policy="missing")
+    builder = build_template_default.get_builder(build_policy="outdated")
     builder.run()
