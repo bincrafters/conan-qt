4c2128747fb0787ed544dc488e640173dc0adf20
bincrafters-user <bincrafters@gmail.com>
2019-12-09 05:08:15 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.15.0

--- conanfile.py
+++ conanfile.py
@@ -264,7 +264,7 @@ class QtConan(ConanFile):
         if self.options.with_sdl2:
             self.requires("sdl2/2.0.9@bincrafters/stable")
         if self.options.with_openal:
-            self.requires("openal/1.19.0@bincrafters/stable")
+            self.requires("openal/1.19.1")
         if self.options.with_libalsa:
             self.requires("libalsa/1.1.9")
         if self.options.GUI and self.settings.os == "Linux":
