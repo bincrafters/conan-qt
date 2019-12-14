731461e89736615ce74ee13aa761a4aa3c07465b
ericLemanissier <ericLemanissier@users.noreply.github.com>
2019-12-14 12:52:51 +0100

    Use default build policy

--- build.py
+++ build.py
@@ -4,5 +4,5 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(build_policy="outdated")
+    builder = build_template_default.get_builder()
     builder.run()
